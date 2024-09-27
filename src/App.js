f7adb77cc0581c7981443330b6aa324723e9114c
amgaina <amgain.abhishek567@gmal.com>
2024-09-26 23:47:45 -0500

    Added loading.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
+import React, { useState, useEffect } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import OpenPage from "./Components/OpenPage/home.js";
 import Paper from "./Components/Paper/paper.js";
 import Blog from "./Components/Blog/blog.js";
+import './App.css';
+import './Components/OpenPage/loading.css'; 
 
+// Loading component
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loader">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+      <h2>Loading...</h2>
+    </div>
+  );
+};
+
+// Main App component
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <HashRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<OpenPage />} />
-          <Route path="/paper" element={<Paper />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Routes>
+            <Route path="/" element={<OpenPage />} />
+            <Route path="/paper" element={<Paper />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        )}
       </div>
     </HashRouter>
   );
