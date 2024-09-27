eab86b0c3d86dd80ea7506eb671d6b71525633bb
amgaina <amgain.abhishek567@gmal.com>
2024-09-26 23:19:12 -0500

    Changed browserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import OpenPage from "./Components/OpenPage/home.js";
 import Paper from "./Components/Paper/paper.js";
 import Blog from "./Components/Blog/blog.js";
-import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<OpenPage />} />
-          <Route path="/paper" element={<Paper />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </div>
-    </Router>
+    <HashRouter>
+      <Routes>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<OpenPage />} />
+            <Route path="/paper" element={<Paper />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </div>
+      </Routes>
+    </HashRouter>
   );
 }
 
