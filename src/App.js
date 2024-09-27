9af145af4d5c55b72d276ce64b73a1c8149f1766
amgaina <amgain.abhishek567@gmal.com>
2024-09-26 22:56:21 -0500

    Files Added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OpenPage from "./Components/OpenPage/home.js";
 import Paper from "./Components/Paper/paper.js";
 import Blog from "./Components/Blog/blog.js";
 import "./App.css";
 
-const Loading = () => {
-  return (
-    <div className="loading-container">
-      <div className="loading-spinner"></div>
-    </div>
-  );
-};
-
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
       <div className="App">
-        {loading ? (
-          <Loading />
-        ) : (
-          <Routes>
-            <Route path="/" element={<OpenPage />} />
-            <Route path="/paper" element={<Paper />} />
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        )}
+        <Routes>
+          <Route path="/" element={<OpenPage />} />
+          <Route path="/paper" element={<Paper />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
       </div>
     </Router>
   );
